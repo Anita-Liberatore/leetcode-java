b010548e7eafdf9193d8c65f3485cd11a86aaf39
Anita Liberatore <anita.liberatore@spindox.it>
2022-12-27 16:52:32 +0100

    add search exercise with binarySearch

---
+++ src/leetcode/Ex4BinarySearch.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+public class Ex4BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int num = 4;
+        System.out.println(search(arr,num));
+
+        int[] arrTwo = {1,2,3,4,6,7};
+        int numTwo = 2;
+        System.out.println(search(arrTwo,numTwo));
+    }
+
+    public static int search(int[] arr, int num) {
+        int start = 0, end = arr.length - 1;
+        int mid = (start + end) / 2;
+
+        while (start <= end) {
+
+            // Check if x is present at mid
+            if (arr[mid] == num)
+                return mid;
+
+            // If x greater, ignore left half
+            if (arr[mid] < num)
+                start = mid + 1;
+
+                // If x is smaller, ignore right half
+            else
+                end = mid - 1;
+
+            mid = (start + end) / 2;
+
+        }
+
+        // if we reach here, then element was
+        // not present
+        return -1;
+    }
+}
