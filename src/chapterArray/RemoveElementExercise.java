10e98436abe2791bcc045fc498b632276abf532a
Anita Liberatore <anitaliberatore@hotmail.com>
2023-01-10 16:58:26 +0100

    RemoveElementExercise

---
+++ src/chapterArray/RemoveElementExercise.java
@@ -0,0 +1,30 @@
+package chapterArray;
+
+/**
+ * Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ *
+ */
+public class RemoveElementExercise {
+
+	public static void main(String[] args) {
+		int[] intArray = {2};
+		int val = 3;
+		System.out.println(removeElement(intArray, val));
+	}
+
+	public static int removeElement(int[] nums, int val) {
+
+		int k = 0;
+
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] != val) {
+				nums[k++] = nums[i];
+			} 
+		}
+
+		return k;
+	}
+}
